7dd7dc936ba642c916c8c98e700f65710d9ce81b
Gigachad0456 <isaacreang05@gmail.com>
2026-04-25 21:20:08 +0530

    new changes created admin and update render.yaml

---
+++ create_admin.py
@@ -0,0 +1,13 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+from core.models import User
+
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+    print("Superuser 'admin' created successfully with password 'admin123'.")
+else:
+    print("Superuser already exists.")
